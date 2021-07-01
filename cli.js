e3c809d0af19390d76123b3fca19505e5e19c38e
alestor123 <alestoraldous@gmail.com>
2021-07-02 00:16:22 +0530

    added main cli code

--- cli.js
+++ cli.js
@@ -1,22 +1,28 @@
 #!/usr/bin/env node
 
-var chalk = require('chalk'),
-brownout = require('./App'),
-{userInfo} = require("os")
+const chalk = require('chalk')
+const brownout = require('./App')
+const { userInfo } = require('os')
 const { basename } = require('path')
-// prompt = require('prompt-sync'),
-// {basename} = require('path')
 const prompt = require('prompt-sync')()
+const cd = basename(process.cwd())
+const usernamee = userInfo().username
+const questions = [{ field: 'username', question: 'Github Username(' + usernamee + ') : ', defaultValue: usernamee, nolengthValidation: true }, { field: 'reponame', question: 'Repository Name (' + cd + ') : ', defaultValue: cd, nolengthValidation: true }, { field: 'token', question: 'Github Token : ' }, { field: 'brname', question: 'Branch : ', nolengthValidation: true }]
 
-// prompt.ask('ded')
 try {
-console.log(brownout(green('Github Username('+userInfo().username+') : ') || userInfo().username, green('Repository Name (' + basename(process.cwd()) + ') : '), green('Github Token : '),green('Branch Name : ') ))
+  const { username, reponame, token, brname } = promptAns()
+  brownout(username, reponame, token, brname)
+  console.log(chalk.green('Done!!'))
+} catch (e) {
+  console.log(chalk.red.bold('Oops we got a problem'))
 }
-catch(e){
-console.log(chalk.red.bold('Oops we got a problem'))
-}
-// will be working on validation 
-function green(qss){
-if(qss && typeof qss === 'string') return prompt(chalk.greenBright.bold(qss))
-else console.error(chalk.redBright.bold('Please enter a valid input'))
+
+function promptAns () {
+  const answers = {}
+  questions.forEach(qes => {
+    const inp = prompt(chalk.greenBright.bold(qes.question))
+    if (qes.defaultValue && qes.nolengthValidation) answers[qes.field] = inp && typeof inp === 'string' && inp.length > 0 ? inp : qes.defaultValue
+    else answers[qes.field] = inp && typeof inp === 'string' && inp.length > 0 ? inp : undefined
+  })
+  return answers
 }
