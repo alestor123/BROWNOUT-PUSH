4160e96d538c85ad1e7763aa581cc31be6f6d735
Alestor Aldous <51041424+alestor123@users.noreply.github.com>
2021-07-02 00:34:34 +0530

    added comment

--- test.js
+++ test.js
@@ -7,4 +7,4 @@ tap.throws(() => bp('eded', '', process.env.GHTOKEN, 'master'), new Error('Enter
 tap.throws(() => bp('dewded', '', 1, 'master'), new Error('Enter a valid repo name'))
 tap.throws(() => bp('eded', 'BROWN-T', 1, 'master'), new Error('Enter a valid token'))
 tap.throws(() => bp('eded', 'BROWN-T', '', 'master'), new Error('Enter a valid token'))
-tap.equal(typeof bp('alestor123', 'BROWNOUT-PUSH', process.env.GHTOKEN, 'master'), 'string')
+tap.equal(typeof bp('alestor123', 'BROWNOUT-PUSH', process.env.GHTOKEN, 'master'), 'string') //not sure about this one
