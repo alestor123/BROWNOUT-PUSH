4bb934ebe743a4eb77424c73ab447c023b2f4f34
alestor123 <alestoraldous@gmail.com>
2021-07-01 20:25:31 +0530

    added test

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-var { execSync } = require('child_process')
-module.exports = (username,repo,token,branch) => {
-    if (!(username && typeof username === 'string' && username.length > 0)) throw new Error('Please enter a vaild username')
-    else if (!(repo && typeof repo === 'string' && repo.length > 0)) throw new Error('Enter a valid repo name')
-    else if (!(token && typeof token === 'string' && token.length > 0)) throw new Error('Enter a valid token') 
-    return execSync('git push -u '+'https://'+token+'@github.com/'+username+'/'+repo+'.git ' + (branch || 'main')).toString()  
+const { execSync } = require('child_process')
+module.exports = (username, repo, token, branch) => {
+  if (!(username && typeof username === 'string' && username.length > 0)) throw new Error('Please enter a vaild username')
+  else if (!(repo && typeof repo === 'string' && repo.length > 0)) throw new Error('Enter a valid repo name')
+  else if (!(token && typeof token === 'string' && token.length > 0)) throw new Error('Enter a valid token')
+  return execSync('git push -u ' + 'https://' + token + '@github.com/' + username + '/' + repo + '.git ' + (branch || 'main')).toString()
 }
---
+++ test.js
@@ -0,0 +1,10 @@
+const tap = require('tap')
+const bp = require('./App')
+
+tap.throws(() => bp(1, 'BROWN-T', process.env.GHTOKEN, 'master'), new Error('Please enter a vaild username'))
+tap.throws(() => bp('', 'BROWN-T', process.env.GHTOKEN, 'master'), new Error('Please enter a vaild username'))
+tap.throws(() => bp('eded', '', process.env.GHTOKEN, 'master'), new Error('Enter a valid repo name'))
+tap.throws(() => bp('dewded', '', 1, 'master'), new Error('Enter a valid repo name'))
+tap.throws(() => bp('eded', 'BROWN-T', 1, 'master'), new Error('Enter a valid token'))
+tap.throws(() => bp('eded', 'BROWN-T', '', 'master'), new Error('Enter a valid token'))
+tap.equal(typeof bp('alestor123', 'BROWN-PUSH', process.env.GHTOKEN, 'master'), 'string')
