8c8a598363fbdb43503338fa58b17f6486d560b2
alestor123 <alestoraldous@gmail.com>
2021-07-01 22:34:24 +0530

    added cli code

--- cli.js
+++ cli.js
@@ -1,1 +1,22 @@
 #!/usr/bin/env node
+
+var chalk = require('chalk'),
+brownout = require('./App'),
+{userInfo} = require("os")
+const { basename } = require('path')
+// prompt = require('prompt-sync'),
+// {basename} = require('path')
+const prompt = require('prompt-sync')()
+
+// prompt.ask('ded')
+try {
+console.log(brownout(green('Github Username('+userInfo().username+') : ') || userInfo().username, green('Repository Name (' + basename(process.cwd()) + ') : '), green('Github Token : '),green('Branch Name : ') ))
+}
+catch(e){
+console.log(chalk.red.bold('Oops we got a problem'))
+}
+// will be working on validation 
+function green(qss){
+if(qss && typeof qss === 'string') return prompt(chalk.greenBright.bold(qss))
+else console.error(chalk.redBright.bold('Please enter a valid input'))
+}
