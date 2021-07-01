7f63d541fe835fff33b8d773f6bbb38f48515193
alestor123 <alestoraldous@gmail.com>
2021-07-02 00:23:17 +0530

    added default branch

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const { basename } = require('path')
 const prompt = require('prompt-sync')()
 const cd = basename(process.cwd())
 const usernamee = userInfo().username
-const questions = [{ field: 'username', question: 'Github Username(' + usernamee + ') : ', defaultValue: usernamee, nolengthValidation: true }, { field: 'reponame', question: 'Repository Name (' + cd + ') : ', defaultValue: cd, nolengthValidation: true }, { field: 'token', question: 'Github Token : ' }, { field: 'brname', question: 'Branch : ', nolengthValidation: true }]
+const questions = [{ field: 'username', question: 'Github Username(' + usernamee + ') : ', defaultValue: usernamee, nolengthValidation: true }, { field: 'reponame', question: 'Repository Name (' + cd + ') : ', defaultValue: cd, nolengthValidation: true }, { field: 'token', question: 'Github Token : ' }, { field: 'brname', question: 'Branch (main) : ', nolengthValidation: true }]
 
 try {
   const { username, reponame, token, brname } = promptAns()
